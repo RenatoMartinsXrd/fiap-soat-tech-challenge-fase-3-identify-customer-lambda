f7867a56623c6f8ee87c019d067ba981be1e53c6
RenatoXrd <almeidarenatosantista@gmail.com>
2025-03-25 15:37:30 -0300

    feat: test banco de dados

--- app/src/main/java/tech/buildrun/lambda/Handler.java
+++ app/src/main/java/tech/buildrun/lambda/Handler.java
@@ -5,7 +5,14 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final String DB_URL = System.getenv("DB_URL") ;
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
                                                       Context context) {
@@ -13,14 +20,50 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
 
         logger.log("Request received - " + request.getBody());
 
-        return new APIGatewayProxyResponseEvent()
-                .withStatusCode(200)
-                .withBody("""
-                        {
-                            "sucesso": true
-                        }
-                        """)
-                .withIsBase64Encoded(false);
+        // Teste de conexão
+        String errorMessage = testDatabaseConnection();
+
+        if (errorMessage != null) {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody("""
+                            {
+                                "error": "Falha na conexão com o banco",
+                                "message": "%s"
+                            }
+                            """.formatted(errorMessage))
+                    .withIsBase64Encoded(false);
+        } else {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody("""
+                            {
+                                "sucesso": "Sucesso ao conectar com o banco"
+                            }
+                            """)
+                    .withIsBase64Encoded(false);
+        }
     }
 
+    private String testDatabaseConnection() {
+        try {
+            // Registra o driver
+            Class.forName("org.postgresql.Driver");
+
+            // Agora você pode tentar se conectar ao banco
+            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                if (connection.isValid(2)) {
+                    return null; // Conexão bem-sucedida
+                } else {
+                    return "Conexão não válida";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Erro ao conectar: " + e.getMessage();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return "Erro: Driver PostgreSQL não encontrado";
+        }
+    }
 }
